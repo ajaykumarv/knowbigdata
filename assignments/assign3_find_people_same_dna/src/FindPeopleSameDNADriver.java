10accd9763358df411765c097a3fc9c002c16a68
Ajay Vundyala <avundyal@github.com>
2016-06-21 20:20:55 -0700

    Add program to find people with same DNA

---
+++ assignments/assign3_find_people_same_dna/src/FindPeopleSameDNADriver.java
@@ -0,0 +1,110 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+
+
+/**
+ * Created by ajay on 6/15/16.
+ */
+public class FindPeopleSameDNADriver {
+    public static class FindPeopleSameDNAMapper extends Mapper<Object, Text, Text, Text> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString(), " \t\"");
+            Text username = null;
+            Text dna_seq = null;
+            if (itr.hasMoreTokens()) {
+                username = new Text(itr.nextToken());
+            }
+            if (itr.hasMoreTokens()) {
+                dna_seq = new Text(itr.nextToken());
+            }
+
+            if (dna_seq != null && username != null){
+                context.write(dna_seq, username);
+            }
+        }
+    }
+
+
+    /**
+     * Created by ajay on 6/15/16.
+     */
+    public static class FindPeopleSameDNAReducer extends Reducer<Text, Text, Text, TextArrayWritable> {
+        private IntWritable result;
+
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            int count = 0;
+            HashSet<String> mySet = new HashSet<String>();
+
+            for(Text val: values) {
+                mySet.add(val.toString());
+            }
+
+            Object[] myArray = mySet.toArray();
+
+            Text[] str_arr = new Text[myArray.length];
+            int i = 0;
+            for(Object s: myArray) {
+                str_arr[i] = new Text(s.toString());
+                i += 1;
+            }
+            TextArrayWritable text_arr = new TextArrayWritable(Text.class, str_arr);
+            context.write(key, text_arr);
+        }
+    }
+
+    /**
+     * Created by ajay on 6/18/16.
+     */
+    public static class TextArrayWritable extends ArrayWritable {
+        public TextArrayWritable(Class<? extends Writable> valueClass) {
+            super(valueClass);
+        }
+
+        public TextArrayWritable(Class<? extends Writable> valueClass, Writable[] values) {
+            super(valueClass, values);
+        }
+
+        @Override
+        public String toString() {
+            Text[] strings = (Text[] )super.get();
+            String output = "";
+            for (Text t: strings) {
+                output = t.toString() + " " + output;
+            }
+            return output;
+        }
+    }
+
+    /**
+     * Created by ajay on 6/15/16.
+     */
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "FindPeopleSameDNA");
+        job.setJarByClass(FindPeopleSameDNADriver.class);
+        job.setMapperClass(FindPeopleSameDNAMapper.class);
+        job.setReducerClass(FindPeopleSameDNAReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setOutputValueClass(TextArrayWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+    }
+}
